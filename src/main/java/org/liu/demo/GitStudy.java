bb5d1ee0d238b1be5fa315acaf8f9d90da0e2133
liu20210425 <liu20210425@163.com>
2022-05-01 09:22:54 +0800

    hot-fix,第一次提交

--- src/main/java/org/liu/demo/GitStudy.java
+++ src/main/java/org/liu/demo/GitStudy.java
@@ -4,5 +4,6 @@ public class GitStudy {
     public static void main(String[] args) {
         System.out.println("再次遇见你，git");
         System.out.println("hello git2");
+        System.out.println("hot-fix hello git1");
     }
 }
