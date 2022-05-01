4543c5aa2e9e073db795ec910aa151b40b517d15
liu20210425 <liu20210425@163.com>
2022-05-01 09:21:26 +0800

    第二次提交

--- src/main/java/org/liu/demo/GitStudy.java
+++ src/main/java/org/liu/demo/GitStudy.java
@@ -3,5 +3,6 @@ package org.liu.demo;
 public class GitStudy {
     public static void main(String[] args) {
         System.out.println("再次遇见你，git");
+        System.out.println("hello git2");
     }
 }
